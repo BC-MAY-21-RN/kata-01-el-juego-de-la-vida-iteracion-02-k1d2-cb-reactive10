5fe62af7bd011936dd5549db5aaf7c069c8472f4
GerardBR3270 <lugo044@gmail.com>
2022-08-03 19:04:06 -0500

    Gerard's update

--- lifeGametry2.js
+++ lifeGametry2.js
@@ -2,8 +2,7 @@ class Board {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.table = new Array();
-        
+        this.table = this.createDimensions();
     }
     createDimensions() {
         let array1 = [];
@@ -20,68 +19,76 @@ class Board {
         return this.table
     }
 
-    findCells(){
+    findCells() {
         let cellsToKill = []
         let cellsToRevive = []
         for (let i = 0; i < this.y; i++) {
             for (let j = 0; j < this.x; j++) {
-            
-                if(this.table[i][j] == 1){
-                    let neighbours = findNeighbours(i,j,this.table)
-                    if(neighbours > 3 || neighbours < 2){
-                        cellsToKill.push([i,j])
+                let neighbours = new Cell(i, j)
+                let count = neighbours.findNeighbours(this.table)
+                if (this.table[i][j] == 1) {
+                    if (count > 3 || count < 2) {
+                        cellsToKill.push([i, j])
                     }
                 }
-                else{
-                    if(findNeighbours(i,j,this.table)== 3){
-                        cellsToRevive.push([i,j])
+                else {
+                    if (neighbours.findNeighbours(this.table) == 3) {
+                        cellsToRevive.push([i, j])
                     }
                 }
             }
         }
-        return {cellsToKill,cellsToRevive}
+        return { cellsToKill, cellsToRevive }
     }
 
-    nextGen(){
+    nextGen() {
         let found = this.findCells();
 
         found.cellsToKill.forEach(cell => {
-            const [x, y] = cell 
-            this.this.table[x][y] = 0
+            const [x, y] = cell
+            this.table[x][y] = 0
         })
 
         found.cellsToRevive.forEach(cell => {
             const [x, y] = cell
-            this.this.table[x][y] = 1
+            this.table[x][y] = 1
         })
     }
 }
 
-class Cell{
-    
-    findNeighbours(x,y,board){
-        let cont = 0
-        for (let i = x-1; i <= x+1; i++) {
-            for (let j = y-1; j <= y+1; j++) {
-                if(i < 0) i= 0
-                if(j <0) j=0 
-                if(i > board.x) break
-                if(j > board.y) break 
+class Cell {
 
-                if([i][j] == 1){
-                    if(i != x || j != y){
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    findNeighbours(board) {
+        let cont = 0
+        for (let i = this.x - 1; i <= this.x + 1; i++) {
+            for (let j = this.y - 1; j <= this.y + 1; j++) {
+                if (i < 0) i = 0
+                if (j < 0) j = 0
+                if (i > board.x) break
+                if (j > board.y) break
+
+                if (board[i][j] == 1) {
+                    if (i != this.x || j != this.y) {
+                        console.log(i + ' ' + j)
                         cont++
                     }
                 }
-            } 
+            }
         }
+        console.log(cont);
         return cont
     }
 }
 
-let firstGen = new Board(4, 8);
+let firstGen = new Board(4, 4);
 
+console.table(firstGen.table)
+firstGen.nextGen()
 console.table(firstGen.table);
-
 
 
